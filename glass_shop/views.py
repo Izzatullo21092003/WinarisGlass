e831936422e1685e69e138235b2af556e0daee33
Izzatullo21092003 <91390212+Izzatullo21092003@users.noreply.github.com>
2022-11-17 22:02:59 +0500

    the required files are created

--- glass_shop/views.py
+++ glass_shop/views.py
@@ -5,3 +5,19 @@ from django.urls import reverse
 from .models import Benson
 
 # Create your views here.
+
+
+def index(request): # layout page for the website
+    pass
+
+
+def login_view(request): # login page of the website
+    pass
+
+
+def logout_view(request): # logout page of the website
+    pass
+
+
+def register_view(request): # register page of the website
+    pass
