e727d7be1db6a343aee4c2fc274263164f0d9bc5
Izzatullo21092003 <91390212+Izzatullo21092003@users.noreply.github.com>
2022-11-21 17:37:10 +0500

    updated project

--- glass_shop/models.py
+++ glass_shop/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 from django.utils.translation import gettext as _
 
 # Create your models here.
+
+
 class Benson(models.Model):
     id = models.BigAutoField(primary_key=True, serialize=False)
     part_number = models.TextField(_("Part Number"), blank=True, null=True)
--- glass_shop/views.py
+++ glass_shop/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -8,16 +9,56 @@ from .models import Benson
 
 
 def index(request): # layout page for the website
-    pass
+    return render(request, "glass_shop/index.html")
 
 
 def login_view(request): # login page of the website
-    pass
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "glass_shop/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "glass_shop/login.html")
 
 
 def logout_view(request): # logout page of the website
-    pass
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
 
 
 def register_view(request): # register page of the website
-    pass
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "glass_shop/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "glass_shop/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "glass_shop/register.html")
